df9b730ff5c1ba9d99708d1459520015832d58ae
dom-inic <dominicnyambane22@gmail.com>
2022-01-28 11:58:45 +0300

    timezone, secret key, migrations fix

--- conshareapi/settings/base.py
+++ conshareapi/settings/base.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'my secret key for local testing'
+SECRET_KEY = 'dp4#6elyt3ecr)quo6di2a)!bhrv)mdf*b&n7cj82t6gwo&yy9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
