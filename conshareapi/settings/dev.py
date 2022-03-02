787b54618a1dda15f06466a2c47b4933b3ee5c52
dom-inic <dominicnyambane22@gmail.com>
2022-03-02 12:33:59 +0300

    postgres db setup

--- conshareapi/settings/dev.py
+++ conshareapi/settings/dev.py
@@ -8,8 +8,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd8153ta0utvn67',
+        'USER': 'tfikjzwhrosoxy',
+        'PASSWORD': '8409a753e4ef9a66b01bdd0544211538179ed057cb58405c221964080b807098',
+        'HOST': 'ec2-54-166-120-40.compute-1.amazonaws.com',
+        'PORT': '5432',
+        
     },
     'message': {
         'ENGINE': 'django.db.backends.sqlite3',
